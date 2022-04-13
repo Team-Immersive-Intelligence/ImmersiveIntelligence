77c1d9873a6ca2d9ed6f05c3ca160e29e416e222
Pabilo8 <pabilo1.pg@gmail.com>
2022-04-13 18:16:15 +0200

    Move small crates to .obj

--- src/main/java/pl/pabilo8/immersiveintelligence/common/blocks/metal/BlockIISmallCrate.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/blocks/metal/BlockIISmallCrate.java
@@ -2,20 +2,22 @@ package pl.pabilo8.immersiveintelligence.common.blocks.metal;
 
 import blusunrize.immersiveengineering.api.IEProperties;
 import blusunrize.immersiveengineering.common.blocks.ItemBlockIEBase;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import pl.pabilo8.immersiveintelligence.common.blocks.BlockIITileProvider;
 import pl.pabilo8.immersiveintelligence.common.blocks.types.IIBlockTypes_SmallCrate;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author Pabilo8
@@ -29,11 +31,8 @@ public class BlockIISmallCrate extends BlockIITileProvider<IIBlockTypes_SmallCra
 		setHardness(3.0F);
 		setResistance(15.0F);
 		lightOpacity = 0;
-		this.setAllNotNormalBlock();
 		setOpaque(false);
-
-		for(IIBlockTypes_SmallCrate b : IIBlockTypes_SmallCrate.values())
-			tesrMap.put(b.getMeta(), "crate");
+		this.setAllNotNormalBlock();
 	}
 
 	@Override
@@ -48,59 +47,29 @@ public class BlockIISmallCrate extends BlockIITileProvider<IIBlockTypes_SmallCra
 		return null;
 	}
 
+	@Nonnull
+	@SuppressWarnings("Deprecation")
+	@Override
+	public Material getMaterial(@Nonnull IBlockState state)
+	{
+		return disableStats().getMetaFromState(state) > 2?Material.IRON: Material.WOOD;
+	}
+
 	@Override
 	public TileEntity createBasicTE(World world, IIBlockTypes_SmallCrate type)
 	{
-		switch(type)
-		{
-			case WOODEN_CRATE_BOX:
-			case WOODEN_CRATE_CUBE:
-			case WOODEN_CRATE_WIDE:
-			case METAL_CRATE_BOX:
-			case METAL_CRATE_CUBE:
-			case METAL_CRATE_WIDE:
-			{
-				return new TileEntitySmallCrate();
-			}
-		}
-		return null;
+		return new TileEntitySmallCrate();
 	}
 
 	@Override
-	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos)
-	{
-		state = super.getActualState(state, world, pos);
-		return state;
-	}
-
-	@Override
-	public boolean allowHammerHarvest(IBlockState state)
+	public boolean allowHammerHarvest(@Nonnull IBlockState state)
 	{
 		return true;
 	}
 
 	@Override
-	public boolean canIEBlockBePlaced(World world, BlockPos pos, IBlockState newState, EnumFacing side, float hitX, float hitY, float hitZ, EntityPlayer player, ItemStack stack)
+	public boolean canIEBlockBePlaced(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState newState, @Nonnull EnumFacing side, float hitX, float hitY, float hitZ, @Nonnull EntityPlayer player, @Nonnull ItemStack stack)
 	{
 		return true;
-	}
-
-	@Override
-	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block blockIn, BlockPos fromPos)
-	{
-		super.neighborChanged(state, world, pos, blockIn, fromPos);
-		TileEntity te = world.getTileEntity(pos);
-	}
-
-	@Deprecated
-	public EnumBlockRenderType getRenderType(IBlockState state)
-	{
-		return EnumBlockRenderType.ENTITYBLOCK_ANIMATED;
-	}
-
-	@Override
-	public boolean isSideSolid(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side)
-	{
-		return false;
 	}
 }
