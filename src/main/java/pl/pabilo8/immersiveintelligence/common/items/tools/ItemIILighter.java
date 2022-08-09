4c1fb13147c0f9b67be3fdbfcc87df1dff444210
justUsingThisAccForWebsite <44799224+lordi-erez@users.noreply.github.com>
2022-08-09 13:26:07 +0300

    moved to commonproxy

--- src/main/java/pl/pabilo8/immersiveintelligence/common/items/tools/ItemIILighter.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/items/tools/ItemIILighter.java
@@ -47,7 +47,7 @@ public class ItemIILighter extends ItemIIBase implements ITool
 	public ItemIILighter()
 	{
 		super("lighter", 1);
-		LighterFuelHandler.addFuel(IEcontent.fluidPlantoil, 100);
+		//LighterFuelHandler.addFuel(IEcontent.fluidPlantoil, 100);
 	}
 
 	/**
