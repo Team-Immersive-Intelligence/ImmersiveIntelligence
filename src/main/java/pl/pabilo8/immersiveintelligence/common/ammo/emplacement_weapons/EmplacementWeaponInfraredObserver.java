c86635a5cdf4f0401b732fbbb1467880abd8b557
H <37481947+H-QueerCoded@users.noreply.github.com>
2023-08-07 15:00:16 +1200

    Revert "Observer aimAt is standard"
    This reverts commit f1bc96472b5a74b756683e83390f32c6195efdf3.

--- src/main/java/pl/pabilo8/immersiveintelligence/common/ammo/emplacement_weapons/EmplacementWeaponInfraredObserver.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/ammo/emplacement_weapons/EmplacementWeaponInfraredObserver.java
@@ -77,7 +77,11 @@ public class EmplacementWeaponInfraredObserver extends EmplacementWeapon
 	@Override
 	public void aimAt(float yaw, float pitch)
 	{
-		super.aimAt(yaw, pitch);
+		//Only pitch, no yaw rotation
+		nextPitch = pitch;
+		float p = pitch-this.pitch;
+		this.pitch += Math.signum(p)*MathHelper.clamp(Math.abs(p), 0, this.getPitchTurnSpeed());
+		this.pitch = this.pitch%180;
 	}
 
 	public boolean isSetUp(boolean door)
