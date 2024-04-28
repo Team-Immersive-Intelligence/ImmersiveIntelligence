c3268ba557c37542ba0d7b690821325633a25985
Avalon <kutsivi2106@gmail.com>
2024-04-28 13:27:20 -0400

    Updating commit notify stuff
    (The code was fixed by Pabilo, excellent work as always)

---
+++ .github/workflows/discord_commit.sh
@@ -0,0 +1,31 @@
+# Usage: discord_commit <URL> <tail commit hash> <head commit hash>
+f=$COMMIT_FROM
+t=$COMMIT_TO
+generate_post_data() {
+cat <<EOF
+{
+  "content": "",
+  "tts": false,
+  "embeds": [
+    {
+      "id": 652627557,
+      "title": "Work Progress Report",
+      "color": 3617648,
+      "description": "We hereby report that the [**Internal Repository**](https://github.com/Team-Immersive-Intelligence/ImmersiveIntelligence-Internal) has received following new commits\\n \
+      $( git log $f...$t --format="[\`%h\`](https://github.com/Team-Immersive-Intelligence/ImmersiveIntelligence-Internal/commit/%h) %s\\n" | tr -d '\n' )",
+      "color": 2631733,
+      "fields": [],
+      "footer": {
+        "text": "Glory to the Engineer Cause!"
+      }
+    }
+  ]
+}
+EOF
+}
+# POST request to Discord Webhook
+curl \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "$(generate_post_data)" \
+  "$WEBHOOK"
