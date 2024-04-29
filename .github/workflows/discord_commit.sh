7bdee900eaf83d4755e1d108daf1a355cff3a7c0
Avalon <54385431+Avalon2106@users.noreply.github.com>
2024-04-29 12:07:41 -0400

    Update discord_commit.sh

--- .github/workflows/discord_commit.sh
+++ .github/workflows/discord_commit.sh
@@ -8,13 +8,9 @@ generate_post_data() {
   "tts": false,
   "embeds": [
     {
-      "id": 652627557,
       "title": "Work Progress Report",
       "color": 3617648,
-      "description": "We hereby report that the [**ImmersiveIntelligence_External**](https://github.com/Team-Immersive-Intelligence/ImmersiveIntelligence) has received following new commits\\n \
-      $(git log "$COMMIT_FROM...$COMMIT_TO" --format="[\`%h\`](https://github.com/Team-Immersive-Intelligence/ImmersiveIntelligence/commit/%h) **%an**: %s\\n" | tr -d '\n' )",
-      "color": 2631733,
-      "fields": [],
+      "description": "We hereby report that the [**ImmersiveIntelligence_External**](https://github.com/Team-Immersive-Intelligence/ImmersiveIntelligence) has received the following new commits\\n$(git log "$COMMIT_FROM...$COMMIT_TO" --format="[\`%h\`](https://github.com/Team-Immersive-Intelligence/ImmersiveIntelligence/commit/%h) **%an**: %s%n%n%b\\n" | tr -d '\n' )",
       "footer": {
         "text": "Glory to the Engineer Cause!"
       }
