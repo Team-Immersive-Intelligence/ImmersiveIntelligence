fe85cbfd83578511b45400039efe736a970b6f49
Avalon <54385431+Avalon2106@users.noreply.github.com>
2024-09-12 13:03:57 -0400

    Re-added WP explosion, status effects
    still needs "shrapnel projectiles"

--- src/main/java/pl/pabilo8/immersiveintelligence/common/ammo/components/incendiary/AmmoComponentWhitePhosphorus.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/ammo/components/incendiary/AmmoComponentWhitePhosphorus.java
@@ -1,14 +1,27 @@
 package pl.pabilo8.immersiveintelligence.common.ammo.components.incendiary;
 
 import blusunrize.immersiveengineering.api.crafting.IngredientStack;
+import blusunrize.immersiveengineering.common.util.IEPotions;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import pl.pabilo8.immersiveintelligence.api.ammo.enums.ComponentEffectShape;
 import pl.pabilo8.immersiveintelligence.api.ammo.enums.ComponentRole;
 import pl.pabilo8.immersiveintelligence.api.ammo.parts.AmmoComponent;
+import pl.pabilo8.immersiveintelligence.common.IIPotions;
+import pl.pabilo8.immersiveintelligence.common.IISounds;
+import pl.pabilo8.immersiveintelligence.common.entity.ammo.component.EntityAtomicBoom;
+import pl.pabilo8.immersiveintelligence.common.entity.ammo.component.EntityWhitePhosphorus;
+import pl.pabilo8.immersiveintelligence.common.network.IIPacketHandler;
 import pl.pabilo8.immersiveintelligence.common.util.IIColor;
+import pl.pabilo8.immersiveintelligence.common.util.IIDamageSources;
+import pl.pabilo8.immersiveintelligence.common.util.IIExplosion;
 
 /**
  * @author Pabilo8
@@ -32,9 +45,27 @@ public class AmmoComponentWhitePhosphorus extends AmmoComponent
 	@Override
 	public void onEffect(World world, Vec3d pos, Vec3d dir, ComponentEffectShape shape, NBTTagCompound tag, float componentAmount, float multiplier, Entity owner)
 	{
-		if(world.isRemote)
-			return;
 
+		BlockPos ppos = new BlockPos(pos);
+		new IIExplosion(world, owner, pos, null, 6*multiplier, 6, ComponentEffectShape.ORB, true, false, false)
+				.doExplosion();
+
+
+
+		EntityLivingBase[] entities = world.getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(ppos).grow(6*multiplier)).toArray(new EntityLivingBase[0]);
+		for(EntityLivingBase e : entities)
+		{
+			e.addPotionEffect(new PotionEffect(IEPotions.flashed, 40, 1));
+			e.addPotionEffect(new PotionEffect(IEPotions.stunned, 40, 1));
+			e.addPotionEffect(new PotionEffect(IEPotions.flammable, 40, 1));
+			e.addPotionEffect(new PotionEffect(IIPotions.brokenArmor, 40, 0));
+		}
+
+		IIPacketHandler.playRangedSound(world, pos, IISounds.explosionIncendiary, SoundCategory.NEUTRAL, 16, 1f, 0f);
+
+		EntityWhitePhosphorus entityWhitePhosphorus = new EntityWhitePhosphorus(world);
+		entityWhitePhosphorus.setPosition(pos.x, pos.y, pos.z);
+		world.spawnEntity(entityWhitePhosphorus);
 
 	}
 }
