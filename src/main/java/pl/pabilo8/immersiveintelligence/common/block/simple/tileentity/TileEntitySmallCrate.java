cf1f24ffec11a978d302689493d6aa670b4a86e4
Avalon <54385431+Avalon2106@users.noreply.github.com>
2024-12-17 17:21:51 -0500

    Fixed #412

--- src/main/java/pl/pabilo8/immersiveintelligence/common/block/simple/tileentity/TileEntitySmallCrate.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/block/simple/tileentity/TileEntitySmallCrate.java
@@ -23,100 +23,95 @@ import javax.annotation.Nullable;
 /**
  * @author Pabilo8
  * @since 2019-05-17
+ * @author Avalon
+ * @since 2024-12-17
  */
-public class TileEntitySmallCrate extends TileEntityWoodenCrate implements IGuiTile, IDirectionalTile, IBlockBounds
-{
-	private static final float[] BOX_X = new float[]{0.1875f, 0, 0.0625f, 0.8125f, 0.625f, 0.9375f};
-	private static final float[] BOX_Z = new float[]{0.0625f, 0, 0.1875f, 0.9375f, 0.625f, 0.8125f};
-	private static final float[] CUBE = new float[]{0.0625f, 0, 0.0625f, 0.9375f, 0.875f, 0.9375f};
-	private static final float[] WIDE_X = new float[]{0.1875f, 0, 0.0625f, 0.8125f, 0.3125f, 0.9375f};
-	private static final float[] WIDE_Z = new float[]{0.0625f, 0, 0.1875f, 0.9375f, 0.3125f, 0.8125f};
+public class TileEntitySmallCrate extends TileEntityWoodenCrate implements IGuiTile, IDirectionalTile, IBlockBounds {
 
+	private static final float[] BOX_X = {0.1875f, 0, 0.0625f, 0.8125f, 0.625f, 0.9375f};
+	private static final float[] BOX_Z = {0.0625f, 0, 0.1875f, 0.9375f, 0.625f, 0.8125f};
+	private static final float[] CUBE = {0.0625f, 0, 0.0625f, 0.9375f, 0.875f, 0.9375f};
+	private static final float[] WIDE_X = {0.1875f, 0, 0.0625f, 0.8125f, 0.3125f, 0.9375f};
+	private static final float[] WIDE_Z = {0.0625f, 0, 0.1875f, 0.9375f, 0.3125f, 0.8125f};
 
 	public EnumFacing facing = EnumFacing.NORTH;
 
 	@Override
-	public void readCustomNBT(@Nonnull NBTTagCompound nbt, boolean descPacket)
-	{
+	public void readCustomNBT(@Nonnull NBTTagCompound nbt, boolean descPacket) {
 		super.readCustomNBT(nbt, descPacket);
-		if(nbt.hasKey("facing"))
-			setFacing(EnumFacing.getFront(nbt.getInteger("facing")));
+		if (nbt.hasKey("facing")) {
+			this.facing = EnumFacing.getFront(nbt.getInteger("facing"));
+		}
+	}
+
+	@Override
+	public void writeCustomNBT(@Nonnull NBTTagCompound nbt, boolean descPacket) {
+		super.writeCustomNBT(nbt, descPacket);
+		nbt.setInteger("facing", this.facing.getIndex());
 	}
 
 	@Override
 	@Nullable
-	public ITextComponent getDisplayName()
-	{
-		return name!=null?new TextComponentString(name): new TextComponentTranslation(IIReference.BLOCK_KEY+"small_crate."+IIBlockTypes_SmallCrate.values()[getBlockMetadata()].getName()+".name");
+	public ITextComponent getDisplayName() {
+		return name != null ? new TextComponentString(name) :
+				new TextComponentTranslation(IIReference.BLOCK_KEY + "small_crate." +
+						IIBlockTypes_SmallCrate.values()[getBlockMetadata()].getName() + ".name");
 	}
 
 	@Override
-	public boolean canOpenGui()
-	{
+	public boolean canOpenGui() {
 		return true;
 	}
 
 	@Override
-	public int getGuiID()
-	{
+	public int getGuiID() {
 		return IIGuiList.GUI_SMALL_CRATE.ordinal();
 	}
 
 	@Override
-	public TileEntity getGuiMaster()
-	{
+	public TileEntity getGuiMaster() {
 		return this;
 	}
 
 	@Override
 	@Nonnull
-	public EnumFacing getFacing()
-	{
+	public EnumFacing getFacing() {
 		return facing;
 	}
 
 	@Override
-	public void setFacing(EnumFacing facing)
-	{
-		this.facing = (facing.getAxis()==Axis.Y)?EnumFacing.NORTH: facing;
+	public void setFacing(EnumFacing facing) {
+		this.facing = (facing.getAxis() == Axis.Y) ? EnumFacing.NORTH : facing;
 	}
 
 	@Override
-	public int getFacingLimitation()
-	{
+	public int getFacingLimitation() {
 		return 2;
 	}
 
 	@Override
-	public boolean mirrorFacingOnPlacement(@Nonnull EntityLivingBase placer)
-	{
+	public boolean mirrorFacingOnPlacement(@Nonnull EntityLivingBase placer) {
 		return true;
 	}
 
 	@Override
-	public boolean canHammerRotate(@Nonnull EnumFacing side, float hitX, float hitY, float hitZ, @Nonnull EntityLivingBase entity)
-	{
+	public boolean canHammerRotate(@Nonnull EnumFacing side, float hitX, float hitY, float hitZ, @Nonnull EntityLivingBase entity) {
 		return true;
 	}
 
 	@Override
-	public boolean canRotate(EnumFacing axis)
-	{
+	public boolean canRotate(EnumFacing axis) {
 		return axis.getAxis().isHorizontal();
 	}
 
 	@Override
 	@Nonnull
-	public float[] getBlockBounds()
-	{
-		switch(world.getBlockState(pos).getValue(IIContent.blockSmallCrate.property))
-		{
+	public float[] getBlockBounds() {
+		switch (world.getBlockState(pos).getValue(IIContent.blockSmallCrate.property)) {
 			case METAL_CRATE_BOX:
 			case WOODEN_CRATE_BOX:
 			case REINFORCED_CRATE_BOX:
-				return facing.getAxis()==Axis.X?
-						BOX_X:
-						BOX_Z;
+				return facing.getAxis() == Axis.X ? BOX_X : BOX_Z;
 			case METAL_CRATE_CUBE:
 			case WOODEN_CRATE_CUBE:
 			case REINFORCED_CRATE_CUBE:
@@ -124,9 +119,7 @@ public class TileEntitySmallCrate extends TileEntityWoodenCrate implements IGuiT
 			case METAL_CRATE_WIDE:
 			case WOODEN_CRATE_WIDE:
 			case REINFORCED_CRATE_WIDE:
-				return facing.getAxis()==Axis.X?
-						WIDE_X:
-						WIDE_Z;
+				return facing.getAxis() == Axis.X ? WIDE_X : WIDE_Z;
 			default:
 				return new float[0];
 		}
