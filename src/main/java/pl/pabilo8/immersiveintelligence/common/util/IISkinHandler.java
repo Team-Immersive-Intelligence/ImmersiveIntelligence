88c100d1976492705322b6422ad92c0e59693cda
Pabilo8 <pabilo@iiteam.net>
2025-08-03 20:10:26 +0200

    Reverted Contributor Skin location to /dev/main branch

--- src/main/java/pl/pabilo8/immersiveintelligence/common/util/IISkinHandler.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/util/IISkinHandler.java
@@ -195,8 +195,7 @@ public class IISkinHandler
 			try
 			{
 				IILogger.info("Attempting to download II special skin list from GitHub");
-				URL url = new URL("https://raw.githubusercontent.com/Pabilo8/ImmersiveIntelligence/dev/pabilo/gui/contributor_skins.json");
-				// URL url = new URL("https://raw.githubusercontent.com/VDeltaGabriel/ImmersiveIntelligenceDEV/master/contributor_skins.json"); // TESTING PURPOSES ONLY
+				URL url = new URL("https://raw.githubusercontent.com/Pabilo8/ImmersiveIntelligence/dev/main/gui/contributor_skins.json");
 				specialSkins.clear();
 				specialSkinsByUUID.clear();
 				JsonStreamParser parser = new JsonStreamParser(new InputStreamReader(url.openStream()));
