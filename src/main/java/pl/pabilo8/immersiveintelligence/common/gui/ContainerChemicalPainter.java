c537578865a7a8c0737389f03db9960a15bbe3f5
Avalon <kutsivi2106@gmail.com>
2025-08-08 17:21:06 -0400

    setup new container for chem painter
    -

--- src/main/java/pl/pabilo8/immersiveintelligence/common/gui/ContainerChemicalPainter.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/gui/ContainerChemicalPainter.java
@@ -5,32 +5,31 @@ import blusunrize.immersiveengineering.common.gui.IESlot;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import pl.pabilo8.immersiveintelligence.common.block.multiblock.metal_multiblock1.tileentity.TileEntityChemicalPainter;
-import pl.pabilo8.immersiveintelligence.common.util.gui.ContainerIIBase;
 
 /**
  * @author Pabilo8 (pabilo@iiteam.net)
  * @since 10.07.2019
  */
-public class ContainerChemicalPainter extends ContainerIIBase<TileEntityChemicalPainter>
+public class ContainerChemicalPainter extends ContainerIEBase<TileEntityChemicalPainter>
 {
-	public Slot[] inputSlot, outputSlot;
-
 	public ContainerChemicalPainter(EntityPlayer player, TileEntityChemicalPainter tile)
 	{
-		super(player, tile);
+		super(player.inventory, tile);
+		//Input/Output Slots
 
-			this.inputSlot = new Slot[2];
-			this.outputSlot = new Slot[2];
+		this.addSlotToContainer(new Slot(this.inv, 0, 13, 13));
+		this.addSlotToContainer(new IESlot.Output(this, this.inv, 1, 13, 59));
 
-			//Item Slots
-			inputSlot[0] = this.addSlotToContainer(new Slot(this.inv, 0, 13, 13));
-			outputSlot[0] = this.addSlotToContainer(new Slot(this.inv, 1, 13, 59));
+		this.addSlotToContainer(new IESlot.FluidContainer(this, this.inv, 2, 137, 21, 0));
+		this.addSlotToContainer(new IESlot.FluidContainer(this, this.inv, 3, 137, 57, 0));
 
-			//Fluid Slots
-			inputSlot[1] = this.addSlotToContainer(new IESlot.FluidContainer(this, this.inv, 2, 137, 21, 0));
-			outputSlot[1] = this.addSlotToContainer(new IESlot.FluidContainer(this, this.inv, 3, 137, 57, 0));
+		this.slotCount = tile.getInventory().size();
+		this.tile = tile;
 
-			//Invin Slots
-			addPlayerInventory(player.inventory, 8 ,141);
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < 9; j++)
+				addSlotToContainer(new Slot(player.inventory, j+i*9+9, 8+j*18, 107+15+i*18));
+		for(int i = 0; i < 9; i++)
+			addSlotToContainer(new Slot(player.inventory, i, 8+i*18, 165+15));
 	}
 }
