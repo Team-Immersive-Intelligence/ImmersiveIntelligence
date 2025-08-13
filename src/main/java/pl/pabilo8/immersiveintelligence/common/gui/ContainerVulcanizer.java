876df24cd195c70774316068635735214eeb76a1
Avalon <kutsivi2106@gmail.com>
2025-08-12 23:49:28 -0400

    Container update batch 1
    Filler and Vulcanizer container update

--- src/main/java/pl/pabilo8/immersiveintelligence/common/gui/ContainerVulcanizer.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/gui/ContainerVulcanizer.java
@@ -4,51 +4,64 @@ import blusunrize.immersiveengineering.common.gui.ContainerIEBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import pl.pabilo8.immersiveintelligence.api.crafting.PrintingRecipe;
+import pl.pabilo8.immersiveintelligence.api.crafting.VulcanizerRecipe;
 import pl.pabilo8.immersiveintelligence.common.block.multiblock.metal_multiblock1.tileentity.TileEntityVulcanizer;
+import pl.pabilo8.immersiveintelligence.common.util.gui.ContainerIIBase;
+
+import javax.annotation.Nonnull;
 
 /**
  * @author Pabilo8 (pabilo@iiteam.net)
  * @since 10.07.2019
  */
-public class ContainerVulcanizer extends ContainerIEBase<TileEntityVulcanizer>
+public class ContainerVulcanizer extends ContainerIIBase<TileEntityVulcanizer>
 {
+	public Slot slotInput, slotOutput;
+
 	public ContainerVulcanizer(EntityPlayer player, TileEntityVulcanizer tile)
 	{
-		super(player.inventory, tile);
+		super(player, tile);
 		//Input/Output Slots
 
-		this.addSlotToContainer(new Slot(this.inv, 0, 6, 19)
+
+		slotInput = this.addSlotToContainer(new Slot(this.inv, 0, 6, 19)
 		{
 			@Override
-			public boolean isItemValid(ItemStack itemStack)
-			{
-				return super.isItemValid(itemStack)&&tile.isStackValid(0, itemStack);
+			public boolean isItemValid(@Nonnull ItemStack stack) {
+				return VulcanizerRecipe.recipeList.values().stream()
+						.anyMatch(recipe ->
+								recipe.input.matches(stack) ||
+										recipe.compoundInput.matches(stack) ||
+										recipe.sulfurInput.matches(stack)
+						);
 			}
 		});
-		this.addSlotToContainer(new Slot(this.inv, 1, 6, 39)
+		slotInput = this.addSlotToContainer(new Slot(this.inv, 1, 6, 39)
 		{
 			@Override
-			public boolean isItemValid(ItemStack itemStack)
-			{
-				return super.isItemValid(itemStack)&&tile.isStackValid(1, itemStack);
+			public boolean isItemValid(@Nonnull ItemStack stack) {
+				return VulcanizerRecipe.recipeList.values().stream()
+						.anyMatch(recipe ->
+								recipe.input.matches(stack) ||
+										recipe.compoundInput.matches(stack) ||
+										recipe.sulfurInput.matches(stack)
+						);
 			}
 		});
-		this.addSlotToContainer(new Slot(this.inv, 2, 6, 59)
+		slotInput = this.addSlotToContainer(new Slot(this.inv, 2, 6, 59)
 		{
 			@Override
-			public boolean isItemValid(ItemStack itemStack)
-			{
-				return super.isItemValid(itemStack)&&tile.isStackValid(2, itemStack);
+			public boolean isItemValid(@Nonnull ItemStack stack) {
+				return VulcanizerRecipe.recipeList.values().stream()
+						.anyMatch(recipe ->
+								recipe.input.matches(stack) ||
+										recipe.compoundInput.matches(stack) ||
+										recipe.sulfurInput.matches(stack)
+						);
 			}
 		});
 
-		this.slotCount = tile.getInventory().size();
-		this.tile = tile;
-
-		for(int i = 0; i < 3; i++)
-			for(int j = 0; j < 9; j++)
-				addSlotToContainer(new Slot(player.inventory, j+i*9+9, 8+j*18, 86+i*18));
-		for(int i = 0; i < 9; i++)
-			addSlotToContainer(new Slot(player.inventory, i, 8+i*18, 144));
+		addPlayerInventory(player.inventory, 8, 141);
 	}
 }
