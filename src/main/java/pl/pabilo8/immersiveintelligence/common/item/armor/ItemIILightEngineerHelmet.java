d05b5be667d18ba1f2245973cb2617e9cdb822d1
Avalon <kutsivi2106@gmail.com>
2026-07-23 14:42:41 -0400

    #677 pt2

--- src/main/java/pl/pabilo8/immersiveintelligence/common/item/armor/ItemIILightEngineerHelmet.java
+++ src/main/java/pl/pabilo8/immersiveintelligence/common/item/armor/ItemIILightEngineerHelmet.java
@@ -1,10 +1,12 @@
 package pl.pabilo8.immersiveintelligence.common.item.armor;
 
+import blusunrize.immersiveengineering.api.Lib;
 import blusunrize.immersiveengineering.api.tool.IElectricEquipment;
 import blusunrize.immersiveengineering.common.util.IEDamageSources.ElectricDamageSource;
 import blusunrize.immersiveengineering.common.util.ItemNBTHelper;
 import com.google.common.collect.Multimap;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -14,6 +16,7 @@ import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -56,6 +59,14 @@ public class ItemIILightEngineerHelmet extends ItemIILightEngineerArmorBase impl
 	public void addInformation(@Nonnull ItemStack stack, @Nullable World world, List<String> list, @Nonnull ITooltipFlag flag)
 	{
 		super.addInformation(stack, world, list, flag);
+		if (hasUpgrade(stack, "engineer_gear") || hasUpgrade(stack, "infiltrator_gear") || hasUpgrade(stack, "technician_gear")) {
+			int energy = getEnergyStored(stack);
+			int max = getMaxEnergyStored(stack);
+			if (max > 0) {
+				String stored = energy + "/" + max;
+				list.add(I18n.format(Lib.DESC + "info.energyStored", TextFormatting.GOLD + stored + TextFormatting.RESET));
+			}
+		}
 	}
 
 	@Override
